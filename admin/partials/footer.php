94852c0e8f04e690d6dd0a01ef339067c020d588
SRB18 <96418351+SRB18@users.noreply.github.com>
2023-04-18 20:14:13 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
